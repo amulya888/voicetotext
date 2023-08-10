01371223ce8864815a4f7d9714212403357537e3
AMULYA SINGH <90597544+amulya888@users.noreply.github.com>
2023-08-10 18:35:02 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+cont_3.addEventListener('click', function () {
+    var speech = true;
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('')
+        document.getElementById("cont_2").innerHTML = transcript;
+        console.log(transcript);
+    });
+
+    if (speech == true) {
+        recognition.start();
+    }
+})
